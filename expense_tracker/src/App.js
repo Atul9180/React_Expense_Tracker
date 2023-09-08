6948f78faee1d1e1302bfa8b092393026e295e37
Atul <patelatulpatel@gmail.com>
2023-09-08 12:30:18 +0530

    Lets start coding

---
+++ expense_tracker/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Expense Items Food</h1>
+        <h2>Food Rs 10</h2>
+        <h2>Petrol Rs 100</h2>
+        <h2>Movies Rs 200</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
